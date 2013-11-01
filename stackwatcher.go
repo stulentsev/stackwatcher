c2b1ce598710507a10683802aeed3f1f4d87eea6
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2013-11-01 09:10:33 +0200

    Initial version (fetches some predefined data)

---
+++ stackwatcher.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+  "bitbucket.org/stulentsev/stackexchange"
+  "fmt"
+  "log"
+  "os/exec"
+  "math/rand"
+  "strconv"
+)
+
+func main() {
+  scrapeQuestions()
+}
+
+func scrapeQuestions() error {
+  var questions []stackexchange.Question
+  wrapper, err := stackexchange.Do("/questions", &questions, &stackexchange.Params{
+    Site:     stackexchange.StackOverflow,
+    Sort:     stackexchange.SortScore,
+    Order:    "asc",
+    PageSize: 3,
+  })
+  if err != nil {
+    return err
+  }
+  
+  fmt.Println(wrapper)
+
+  for _, question := range questions {
+    fmt.Printf("%s (ID=%d)\n", question.Title, question.ID)
+    notify(question.Title, question.Link)
+  }
+  return nil
+}
+
+func notify(msg string, url string) {
+  group := strconv.Itoa(rand.Int())
+  fmt.Println(group)
+  cmd := exec.Command("terminal-notifier",
+    "-title", "Greeting",
+    "-group", "a1",
+    "-message", fmt.Sprintf("Hello: %s", msg),
+    "-sound", "Ping",
+    "-open", url,
+  )
+
+  err := cmd.Run()
+  if err != nil {
+    log.Fatal(err)
+  }
+}
