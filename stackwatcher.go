1e7610c76ecbd2f7eeec257a37a58fdd7677e366
Sergio Tulentsev <sergei.tulentsev@gmail.com>
2014-01-15 13:41:10 +0400

    latest changes

--- stackwatcher.go
+++ stackwatcher.go
@@ -1,50 +1,49 @@
 package main
 
 import (
-  "bitbucket.org/stulentsev/stackexchange"
-  "fmt"
-  "net/http"
+	"bitbucket.org/stulentsev/stackexchange"
+	"fmt"
+	"net/http"
 )
 
-
 func main() {
-  output := makePipeline(poller, filterer, onlyUnseen, markSeen)
+	output := makePipeline(poller, filterer, onlyUnseen, markSeen)
 
-  notifier(output)
+	notifier(output)
 }
 
 func fetchLatestQuestions() ([]Question, error) {
-  client := stackexchange.Client{
-    Client: http.DefaultClient,
-    AccessToken: "s*b0dcp5xmcO*lDg5IAj6w))",
-    Key: "trkYWI2VTOWLi05pPpIulw((",
-  }
-  
-  var questions []stackexchange.Question
-  wrapper, err := client.Do("/questions", &questions, &stackexchange.Params{
-    Site:  stackexchange.StackOverflow,
-    Sort:  stackexchange.SortCreationDate,
-    Order: "desc",
+	client := stackexchange.Client{
+		Client:      http.DefaultClient,
+		AccessToken: "s*b0dcp5xmcO*lDg5IAj6w))",
+		Key:         "trkYWI2VTOWLi05pPpIulw((",
+	}
 
-    PageSize: 50,
-  })
-  
-  fmt.Printf("Backoff: %v\n", wrapper.Backoff)
-  fmt.Printf("Quota: %d / %d\n", wrapper.QuotaRemaining, wrapper.QuotaMax)
+	var questions []stackexchange.Question
+	wrapper, err := client.Do("/questions", &questions, &stackexchange.Params{
+		Site:  stackexchange.StackOverflow,
+		Sort:  stackexchange.SortCreationDate,
+		Order: "desc",
 
-  if err != nil {
-    fmt.Println(err)
-    return nil, err
-  }
+		PageSize: 50,
+	})
 
-  var result []Question = make([]Question, len(questions))
-  for i, question := range questions {
-    result[i] = Question{
-      ID:    question.ID,
-      Title: question.Title,
-      Tags:  question.Tags,
-      Link:  question.Link,
-    }
-  }
-  return result, nil
+	fmt.Printf("Backoff: %d\n", wrapper.Backoff)
+	fmt.Printf("Quota: %d / %d\n", wrapper.QuotaRemaining, wrapper.QuotaMax)
+
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+
+	var result []Question = make([]Question, len(questions))
+	for i, question := range questions {
+		result[i] = Question{
+			ID:    question.ID,
+			Title: question.Title,
+			Tags:  question.Tags,
+			Link:  question.Link,
+		}
+	}
+	return result, nil
 }
